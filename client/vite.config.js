fd9b90b3d752ee1d8e9b9632a84906ede3e9893a
Arvind Prajapati <arvindprajapatijan86@gmail.com>
2025-07-15 21:56:14 +0530

    fix: vercel routing and vite base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensures Vite doesn't break routing on Vercel
   build: {
-    outDir: 'dist',  // output directory for Vercel to serve
-  },
-  base: './',        // important for loading assets correctly on Vercel
+    outDir: 'dist'
+  }
 })
